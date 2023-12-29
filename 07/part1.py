c4a206b02cd8956d10ee28b90c036c009697c967
genomesh <dragon.maxen@gmail.com>
2023-12-29 15:40:43 +0000

    day 7 part 1

--- 07/part1.py
+++ 07/part1.py
@@ -1,28 +1,70 @@
+import numpy as np
+
 file = open("input.txt", "r")
 
-times = []
-record_distances = []
+hands = []
 
-def parse_line(line):
-    return list(
-        map(lambda x: int(x.strip()),
-        filter(lambda x: x.strip() != '',
-        line.split(' ')[1:]
-    )))
+class hand_object:
+    def __init__(self, hand, bid, type, value):
+        self.hand = hand
+        self.bid = bid
+        self.type = type
+        self.value = value
 
+def get_card_value(card):
+    if card == 'A':
+        return 12
+    if card == 'K':
+        return 11
+    if card == 'Q':
+        return 10
+    if card == 'J':
+        return 9
+    if card == 'T':
+        return 8
+    return int(card) - 2
 
-times, record_distances = map(parse_line, file.readlines())
+def get_hand_value(hand):
+    value = 0
+    for i, card in enumerate(hand):
+        value += 13 ** (4-i) * get_card_value(card)
+    return value
 
-answer = 1
+possible_card_types = ['five', 'four', 'full house', 'three', 'two pair', 'one pair', 'high card']
 
-for time, record in zip(times, record_distances):
-    ways_to_win = 0
-    for time_held in range(1, time):
-        distance = time_held * (time - time_held)
-        if distance > record:
-            ways_to_win += 1
-    print(ways_to_win)
-    answer *= ways_to_win
+def get_type(hand):
+    card_counts = []
+    for card in hand:
+        card_counts.append(hand.count(card))
+    if max(card_counts) == 5:
+        return 'five'
+    if max(card_counts) == 4:
+        return 'four'
+    if max(card_counts) == 3 and min(card_counts) == 2:
+        return 'full house'
+    if max(card_counts) == 3:
+        return 'three'
+    if card_counts.count(2) == 4:
+        return 'two pair'
+    if max(card_counts) == 2:
+        return 'one pair'
+    return 'high card'
+
+for line in file.readlines():
+    hand, bid = line.split(' ')
+    hands.append(hand_object(hand, int(bid.strip()), get_type(hand), get_hand_value(hand)))
+
+answer = 0
+current_rank = 1
+
+for t in reversed(possible_card_types):
+    hands_of_type = list(filter(lambda x: x.type == t, hands))
+    hand_values = []
+    for hand in hands_of_type:
+        hand_values.append(hand.value)
+    for index in np.argsort(hand_values):
+        answer += current_rank * hands_of_type[index].bid
+        current_rank += 1
 
 print(answer)
 
