bb695061348993f33e50febafe3d1af73e842040
genomesh <dragon.maxen@gmail.com>
2023-12-29 11:58:27 +0000

    part 1 day 6

--- 06/part1.py
+++ 06/part1.py
@@ -1,47 +1,29 @@
 file = open("input.txt", "r")
 
-answer = 0
-seeds = []
-map_objs = []
+times = []
+record_distances = []
 
-class map_obj:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
-        self.maps = []
-    
-    def get_dest_id(self, source_id):
-        for m in self.maps:
-            
-            if source_id >= m['source_start'] and source_id <  m['source_start'] + m['range']:
-                return source_id - m['source_start'] + m['dest_start']
-            
-        return(source_id)
-    
+def parse_line(line):
+    return list(
+        map(lambda x: int(x.strip()),
+        filter(lambda x: x.strip() != '',
+        line.split(' ')[1:]
+    )))
 
-for line in file.readlines():
-    if line.strip() == '':
-        continue
-    if line[0:6] == 'seeds:':
-        seeds = list(map(int, line.strip().split(' ')[1:]))
-        continue
-    if ':' in line:
-        source, destination = line.split('-')[0:3:2]
-        map_objs.append(map_obj(source, destination.split(' ')[0]))
-    else:
-        keys = ['dest_start', 'source_start', 'range']
-        values = map(int, line.strip().split(' '))
-        d = dict(zip(keys, values))
-        map_objs[-1].maps.append(d)
 
-final_location = seeds.copy()
+times, record_distances = map(parse_line, file.readlines())
 
-for id, seed in enumerate(seeds):
-    dest = seed
-    for m in map_objs:
-        dest = m.get_dest_id(dest)
-    final_location[id] = dest
+answer = 1
 
-print(min(final_location))
+for time, record in zip(times, record_distances):
+    ways_to_win = 0
+    for time_held in range(1, time):
+        distance = time_held * (time - time_held)
+        if distance > record:
+            ways_to_win += 1
+    print(ways_to_win)
+    answer *= ways_to_win
+
+print(answer)
 
 file.close()
--- 06/part2.py
+++ 06/part2.py
@@ -1,54 +1,29 @@
 file = open("input.txt", "r")
 
-seed_inits = []
-seed_ranges = []
-map_objs = []
+times = []
+record_distances = []
 
-class map_obj:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
-        self.maps = []
-    
-    def get_source_id(self, dest_id):
-        for m in self.maps:
-            if dest_id >= m['dest_start'] and dest_id <  m['dest_start'] + m['range']:
-                return dest_id + m['source_start'] - m['dest_start']
-        return dest_id
+def parse_line(line):
+    return list(
+        map(lambda x: int(x.strip()),
+        filter(lambda x: x.strip() != '',
+        line.split(' ')[1:]
+    )))
 
-for line in file.readlines():
-    if line.strip() == '':
-        continue
-    if line[0:6] == 'seeds:':
-        seeds_and_ranges = list(map(int, line.strip().split(' ')[1:]))
-        l = len(seeds_and_ranges)
-        seed_inits = seeds_and_ranges[0:l:2]
-        seed_ranges = seeds_and_ranges[1:l:2]
-        continue
-    if ':' in line:
-        source, destination = line.split('-')[0:3:2]
-        map_objs.append(map_obj(source, destination.split(' ')[0]))
-    else:
-        keys = ['dest_start', 'source_start', 'range']
-        values = map(int, line.strip().split(' '))
-        d = dict(zip(keys, values))
-        map_objs[-1].maps.append(d)
 
-def seed_exists(dest):
-    seed = dest
-    for m in reversed(map_objs):
-        seed = m.get_source_id(seed)
-    for seed_init, seed_range in zip(seed_inits, seed_ranges):
-        if (seed_init <= seed and seed_init + seed_range > seed):
-            return True
-    return False
+times, record_distances = map(parse_line, file.readlines())
 
-final_location = 0
+answer = 1
 
-while not seed_exists(final_location):
-    final_location += 1
-    if final_location % 100000 == 0:
-        print(final_location)
-print(final_location)
+for time, record in zip(times, record_distances):
+    ways_to_win = 0
+    for time_held in range(1, time):
+        distance = time_held * (time - time_held)
+        if distance > record:
+            ways_to_win += 1
+    print(ways_to_win)
+    answer *= ways_to_win
+
+print(answer)
 
 file.close()
