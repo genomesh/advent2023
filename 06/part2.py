3b9f5a57202a4152e637af1296e6d7e6106aa3fc
genomesh <dragon.maxen@gmail.com>
2023-12-29 12:06:43 +0000

    day 6 complete

--- 06/part2.py
+++ 06/part2.py
@@ -4,26 +4,26 @@ times = []
 record_distances = []
 
 def parse_line(line):
-    return list(
-        map(lambda x: int(x.strip()),
+    return int(
+        "".join(
+        map(lambda x: x.strip(),
         filter(lambda x: x.strip() != '',
         line.split(' ')[1:]
-    )))
+    ))))
 
+time, record_distance = map(parse_line, file.readlines())
 
-times, record_distances = map(parse_line, file.readlines())
+ways_to_win = 0
 
-answer = 1
+smallest_time_held = 0
+largest_time_held = time
 
-for time, record in zip(times, record_distances):
-    ways_to_win = 0
-    for time_held in range(1, time):
-        distance = time_held * (time - time_held)
-        if distance > record:
-            ways_to_win += 1
-    print(ways_to_win)
-    answer *= ways_to_win
+while smallest_time_held * (time - smallest_time_held) <= record_distance:
+    smallest_time_held += 1
 
-print(answer)
+while largest_time_held * (time - largest_time_held) <= record_distance:
+    largest_time_held -= 1
+
+print(largest_time_held - smallest_time_held + 1)
 
 file.close()
