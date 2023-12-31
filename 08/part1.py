1cd14a2cccc2cb054cad263fb2e4c5489e22b34f
genomesh <dragon.maxen@gmail.com>
2023-12-31 12:06:14 +0000

    day 8 part 1 complete

--- 08/part1.py
+++ 08/part1.py
@@ -2,70 +2,31 @@ import numpy as np
 
 file = open("input.txt", "r")
 
-hands = []
-
-class hand_object:
-    def __init__(self, hand, bid, type, value):
-        self.hand = hand
-        self.bid = bid
-        self.type = type
-        self.value = value
-
-def get_card_value(card):
-    if card == 'A':
-        return 12
-    if card == 'K':
-        return 11
-    if card == 'Q':
-        return 10
-    if card == 'J':
-        return 9
-    if card == 'T':
-        return 8
-    return int(card) - 2
-
-def get_hand_value(hand):
-    value = 0
-    for i, card in enumerate(hand):
-        value += 13 ** (4-i) * get_card_value(card)
-    return value
-
-possible_card_types = ['five', 'four', 'full house', 'three', 'two pair', 'one pair', 'high card']
-
-def get_type(hand):
-    card_counts = []
-    for card in hand:
-        card_counts.append(hand.count(card))
-    if max(card_counts) == 5:
-        return 'five'
-    if max(card_counts) == 4:
-        return 'four'
-    if max(card_counts) == 3 and min(card_counts) == 2:
-        return 'full house'
-    if max(card_counts) == 3:
-        return 'three'
-    if card_counts.count(2) == 4:
-        return 'two pair'
-    if max(card_counts) == 2:
-        return 'one pair'
-    return 'high card'
+nodes = {}
+instructions = ''
+len_instructions = 0
+# is 263, which is prime
 
 for line in file.readlines():
-    hand, bid = line.split(' ')
-    hands.append(hand_object(hand, int(bid.strip()), get_type(hand), get_hand_value(hand)))
+    if '=' in line:
+        l = line.split(' ')
+        this_node = l[0]
+        left = l[2][1:4]
+        right = l[3][0:3]
+        nodes[this_node] = {'L': left, 'R': right}
+    elif 'L' in line:
+        instructions = line.strip()
+        len_instructions = len(instructions)
 
-answer = 0
-current_rank = 1
+location = 'AAA'
+number_of_moves = 0
+instruction_num = 0
+while location != 'ZZZ':
+    instruction = instructions[instruction_num]
+    location = nodes[location][instruction]
+    number_of_moves += 1
+    instruction_num = (instruction_num + 1) % len_instructions
 
-for t in reversed(possible_card_types):
-    hands_of_type = list(filter(lambda x: x.type == t, hands))
-    hand_values = []
-    for hand in hands_of_type:
-        hand_values.append(hand.value)
-    for index in np.argsort(hand_values):
-        answer += current_rank * hands_of_type[index].bid
-        current_rank += 1
-
-print(answer)
+print(number_of_moves)
 
 file.close()
--- 08/part2.py
+++ 08/part2.py
@@ -1,87 +1,51 @@
 import numpy as np
 
-file = open("test.txt", "r")
+file = open("input.txt", "r")
 
-hands = []
-
-class hand_object:
-    def __init__(self, hand, bid, type, value):
-        self.hand = hand
-        self.bid = bid
-        self.type = type
-        self.value = value
-
-def get_card_value(card):
-    if card == 'J':
-        return 0
-    if card == 'A':
-        return 12
-    if card == 'K':
-        return 11
-    if card == 'Q':
-        return 10
-    if card == 'T':
-        return 9
-    return int(card) - 1
-
-def get_hand_value(hand):
-    value = 0
-    for i, card in enumerate(hand):
-        value += 13 ** (4-i) * get_card_value(card)
-    return value
-
-possible_card_types = ['five', 'four', 'full house', 'three', 'two pair', 'one pair', 'high card']
-
-def get_type_no_joker(hand):
-    card_counts = []
-    for card in hand:
-        card_counts.append(hand.count(card))
-    if max(card_counts) == 5:
-        return 'five'
-    if max(card_counts) == 4:
-        return 'four'
-    if max(card_counts) == 3 and min(card_counts) == 2:
-        return 'full house'
-    if max(card_counts) == 3:
-        return 'three'
-    if card_counts.count(2) == 4:
-        return 'two pair'
-    if max(card_counts) == 2:
-        return 'one pair'
-    return 'high card'
-
-def get_type(hand):
-    num_jokers = hand.count('J')
-    if num_jokers == 0:
-        return get_type_no_joker(hand)
-    if num_jokers == 5:
-        return 'five'
-    # assumption: always best to add Joker to most populous card type.
-    # find most common character
-    other_cards = list(filter(lambda x: x != 'J', hand))
-    # either 1, 2, 3, 4 other cards
-    card_counts = []
-    for card in other_cards:
-        card_counts.append(hand.count(card))
-    most_common_card = other_cards[np.argmax(card_counts)]
-    return get_type_no_joker(hand.replace('J', most_common_card))
+nodes = {}
+instructions = ''
+len_instructions = 0
+start_locations = []
+end_locations = []
 
 for line in file.readlines():
-    hand, bid = line.split(' ')
-    hands.append(hand_object(hand, int(bid.strip()), get_type(hand), get_hand_value(hand)))
+    if '=' in line:
+        l = line.split(' ')
+        this_node = l[0]
+        if this_node[2] == 'A':
+            start_locations.append(this_node)
+        if this_node[2] == 'Z':
+            end_locations.append(this_node)
+        left = l[2][1:4]
+        right = l[3][0:3]
+        nodes[this_node] = {'L': left, 'R': right}
+    elif 'L' in line:
+        instructions = line.strip()
+        len_instructions = len(instructions)
 
-answer = 0
-current_rank = 1
+locations = start_locations
+print(f'{len(locations)} start locations')
 
-for t in reversed(possible_card_types):
-    hands_of_type = list(filter(lambda x: x.type == t, hands))
-    hand_values = []
-    for hand in hands_of_type:
-        hand_values.append(hand.value)
-    for index in np.argsort(hand_values):
-        answer += current_rank * hands_of_type[index].bid
-        current_rank += 1
+def at_end_locations(locations):
+    for location in locations:
+        if location not in end_locations:
+            return False
+    return True
 
-print(answer)
+number_of_moves = 0
+instruction_num = 0
+
+while not at_end_locations(locations):
+    instruction = instructions[instruction_num]
+    locations = [nodes[location][instruction] for location in locations]
+    number_of_moves += 1
+    instruction_num = (instruction_num + 1) % len_instructions
+    if number_of_moves % 1000000 == 0:
+        print(number_of_moves)
+
+print(f'answer: {number_of_moves}')
 
 file.close()
+
+#strategy: try to identify cycles
+# 
